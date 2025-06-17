3521964409400b0a8cc251d89cde4556c1c8b9ce
mh-millat <millatsarker224@gmail.com>
2025-06-17 14:27:24 +0600

    added the eight code

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-// import TrustedOrgs from "../components/TrustedOrgs";
 import Footer from "../components/Footer";
 
 const MainLayout = () => {
@@ -10,7 +9,6 @@ const MainLayout = () => {
       <main className="flex-1">
         <Outlet />
       </main>
-      {/* <TrustedOrgs></TrustedOrgs> */}
       <Footer></Footer>
     </div>
   );
